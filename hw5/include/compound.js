1b8adfc5075813515e9b768908b3a925908f7917
vertz <winter333@gmail.com>
2014-01-12 01:48:11 +0200

    Create compound.js

---
+++ hw5/include/compound.js
@@ -0,0 +1,10 @@
+use agg
+db.products.aggregate([
+    {$group:
+      {
+        _id: { "manufacturer":"$manufacturer",
+               "category" : "$category"},
+        num_products:{$sum:1}
+      }
+    }
+])
